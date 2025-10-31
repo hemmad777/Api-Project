002296d84c468cb2af3fdf61913569dee35a357a
Umar Hemmad <hemmad777@gmail.com>
2025-10-31 15:07:24 +0530

    server setted on 3000 port

---
+++ app.js
@@ -0,0 +1,23 @@
+const express=require("express");
+const mongoose=require("mongoose");
+const dotenv=require("dotenv");
+dotenv.config();
+const app=express();
+
+app.use(express.json())
+
+mongoose.connect("mongodb://localhost:27017/Ecomerce")
+.then(()=>{
+    console.log("Mongodb connection successfully");
+    
+})
+.catch((err)=>{
+    console.log("Error is:"+err.message);
+    
+})
+
+app.listen(3005,()=>{
+    console.log("Server started for running at 3005 Port");
+    
+}
+)
