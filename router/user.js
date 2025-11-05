d245b0f27077edab37480c003844d72487a6c83a
Umar Hemmad <hemmad777@gmail.com>
2025-11-05 18:31:36 +0530

    token verify middleware added to user router

--- router/user.js
+++ router/user.js
@@ -2,13 +2,14 @@ const express = require("express");
 const userRoute = express.Router();
 
 const { register,login,userDeleteById,userGetById,userUpdateById } = require("../controller/user");
+const {verifyToken}=require("../middleware/auth")
 
 
 userRoute.post("/register", register);
 userRoute.post("/login",login);
-userRoute.get("/:id",userGetById);
-userRoute.put("/:id",userUpdateById);
-userRoute.delete("/:id",userDeleteById);
+userRoute.get("/:id",verifyToken,userGetById);
+userRoute.put("/:id",verifyToken,userUpdateById);
+userRoute.delete("/:id",verifyToken,userDeleteById);
 
 
 // Define other routes here
