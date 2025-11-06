6330665790b14b243244241b61043cf9fdb87433
Umar Hemmad <hemmad777@gmail.com>
2025-11-06 22:50:59 +0530

    Token verification added to cart routers

--- router/cart.js
+++ router/cart.js
@@ -1,10 +1,11 @@
 const express=require("express");
 const cartRoute=express.Router();
 const {postCart,getAllCart,deleteCart,deleteOne}=require("../controller/cart");
+const {verifyToken}=require("../middleware/auth")
 
-cartRoute.post("/add",postCart);
-cartRoute.get("/all",getAllCart);
-cartRoute.delete("/delete",deleteCart);
-cartRoute.delete("/:productId",deleteOne);
+cartRoute.post("/add",verifyToken,postCart);
+cartRoute.get("/all",verifyToken,getAllCart);
+cartRoute.delete("/delete",verifyToken,deleteCart);
+cartRoute.delete("/:productId",verifyToken,deleteOne);
 
 module.exports=cartRoute;
