d7cd5156c31e4f15685bd53fc6c04c78dacc43bb
Umar Hemmad <hemmad777@gmail.com>
2025-11-06 22:48:57 +0530

    token verify added to product routers

--- router/product.js
+++ router/product.js
@@ -2,10 +2,11 @@ const express=require("express");
 const productRoute=express.Router();
 
 const {getAllProducts,getProductById,productSearch}=require("../controller/product");
+const {verifyToken}=require("../middleware/auth");
 
-productRoute.get("/all",getAllProducts);
-productRoute.get("/search",productSearch);
-productRoute.get("/:id",getProductById);
+productRoute.get("/all",verifyToken,getAllProducts);
+productRoute.get("/search",verifyToken,productSearch);
+productRoute.get("/:id",verifyToken,getProductById);
 
 
 module.exports=productRoute;
