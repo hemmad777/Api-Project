91e99709e298d44d57ca25f937ae9d999276e29d
Umar Hemmad <hemmad777@gmail.com>
2025-11-07 06:36:50 +0530

    Order model created

---
+++ model/order.js
@@ -0,0 +1,36 @@
+const { timeStamp } = require("console");
+const mongoose=require("mongoose");
+
+const orderSchema=new mongoose.Schema({
+    userId:{
+        type:String,
+        required:[true,"UserId not provided"],
+    },
+    items:{
+        type:Object,
+        required:[true,"Cart is also empty"],
+    },
+    totalAmount:{
+        type:Number,
+        required:[true,"Provide total amount"],
+        min:[0,"provde greater than 0"],
+    },
+    status:{
+        type:String,
+        required:[true,"provide the status"],
+    },
+    rozorpayOrderId:{
+        type:String,
+        required:[true,"rozorpayOrderId not provided"],
+    },
+    rozorpayPaymentId:{
+        type:String,
+        required:[true,"provide rozorpay Id"],
+    },
+    shippingAddress:{
+        type:Object,
+        required:[true,"shipping address not provided"]
+    }
+},{timestamps:true});
+
+module.exports=mongoose.model(orderSchema,"order");
