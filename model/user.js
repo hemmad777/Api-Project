8d8cd482fb4fa824d9833709c9c1e64b8d321af1
Umar Hemmad <hemmad777@gmail.com>
2025-11-07 06:47:27 +0530

    Role deleter form the  user model

--- model/user.js
+++ model/user.js
@@ -19,10 +19,6 @@ const userSchema=new mongoose.Schema({
         required:[true,"Please enter password"],
         minLength:[8,"Enter atleast 8 characters"]
     },
-    role:{
-        type:String,
-        required:[true,"Please provide your role"]
-    },
     createdAt:{
         type:Date,
         default:Date.now
