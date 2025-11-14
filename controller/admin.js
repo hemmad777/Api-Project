b9def1067bc4cbef1803046caa093954b7f8db46
Umar Hemmad <hemmad777@gmail.com>
2025-11-14 20:35:21 +0530

    create product creation logic for Admin

--- controller/admin.js
+++ controller/admin.js
@@ -1,5 +1,8 @@
+const { strict } = require("assert");
 const Users=require("../model/user");
-const bcrypt=require("bcrypt")
+const bcrypt=require("bcrypt");
+const slugify=require("slugify");
+const Products=require("../model/product")
 
 // Logic for created Admin
 exports.createAdmin=async (req,res)=>{
@@ -103,5 +106,48 @@ exports.userUpdateById=async (req,res)=>{
     
     } catch (error) {
         return res.status(500).json({message:"Error happened when run is "+error.message})
+    }
+}
+
+// logic for post a product
+
+exports.createProduct=async(req,res)=>{
+    try {
+        const {name,description,price,stock,category,imageUrl,isPublished}=req.body;
+        const Admin=await Users.findById(req.user.userId);
+
+        if(!name||!description||!price||!stock||!category||!imageUrl){
+            return res.status(400).json({message:"Not provided all required field"});
+        }
+
+
+        if(!Admin){
+            return res.status(404).json({message:"Not found admin with this id"});
+        }
+
+        if(Admin.role!="Admin"){
+            return res.status(401).json({message:"Admins and sellers only can add product"});
+        }
+
+        const slug=slugify(name,{lower:true,strict:true});
+
+        const product=new Products({
+            name,
+            slug,
+            description,
+            price,
+            stock,
+            category,
+            imageUrl,
+            isPublished:isPublished||false
+        })
+
+        await product.save();
+
+        res.status(200).json({message:"Successfully added the product",product:product});
+
+
+    } catch (error) {
+        res.status(500).json({message:error.message});
     }
 }
--- router/admin.js
+++ router/admin.js
@@ -1,11 +1,12 @@
 const express=require("express");
 const adminRoute=express.Router();
-const {createAdmin,getAllUsers}=require("../controller/admin");
+const {createAdmin,getAllUsers,createProduct}=require("../controller/admin");
 const {verifyToken}=require("../middleware/auth");
 
 
-adminRoute.post("/create",verifyToken,createAdmin);
+adminRoute.post("/create/admin",verifyToken,createAdmin);
 adminRoute.get("/users",verifyToken,getAllUsers);
+adminRoute.post("/create/product",verifyToken,createProduct)
 
 
 module.exports=adminRoute;
