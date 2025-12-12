0a4582da5784cf4c5b949826cda6347d20483c55
Umar Hemmad <hemmad777@gmail.com>
2025-12-13 01:09:44 +0530

    added role checking inside admin routers

--- router/admin.js
+++ router/admin.js
@@ -5,15 +5,42 @@ const {verifyToken}=require("../middleware/auth");
 
 const multer=require("multer");
 const storage=require("../middleware/upload");
+const { roleChecking } = require("../middleware/role");
 const uploadimage=(folder)=> multer({storage:storage(folder)})
 
 
-adminRoute.post("/create/admin",verifyToken,createAdmin);
+adminRoute.post(
+    "/create/admin",
+    verifyToken,
+    roleChecking("admin"),
+    createAdmin
+);
 // adminRoute.post("/create/product",verifyToken,createProduct);
-adminRoute.put("/user/edit/:id",verifyToken,userUpdateById);
-adminRoute.post("/create/category",verifyToken,uploadimage("images/categories").single("image"),createCategory);
-adminRoute.get("/categories",getAllCategories);
-adminRoute.get("/users",verifyToken,getAllUsers);
+adminRoute.put(
+    "/user/edit/:id",
+    verifyToken,
+    roleChecking("admin"),
+    userUpdateById
+);
+adminRoute.post(
+    "/create/category",
+    verifyToken,
+    roleChecking("admin"),
+    uploadimage("images/categories").single("image"),
+    createCategory
+);
+adminRoute.get(
+    "/categories",
+    verifyToken,
+    roleChecking("admin"),
+    getAllCategories
+);
+adminRoute.get(
+    "/users",
+    verifyToken,
+    roleChecking("admin"),
+    getAllUsers
+);
 
 
 
