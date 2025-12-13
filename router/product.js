3b6140863e5e956f56641f2459e25d16d1f94ac6
Umar Hemmad <hemmad777@gmail.com>
2025-12-13 11:29:00 +0530

    Added featured in product router

--- router/product.js
+++ router/product.js
@@ -1,12 +1,13 @@
 const express=require("express");
 const productRoute=express.Router();
 
-const {getAllProducts,getProductById,productSearch}=require("../controller/product");
+const {getAllProducts,getProductById,productSearch,featuredProducts}=require("../controller/product");
 const {verifyToken}=require("../middleware/auth");
 
 productRoute.get("/all",verifyToken,getAllProducts);
 productRoute.get("/search",verifyToken,productSearch);
 productRoute.get("/:id",verifyToken,getProductById);
+productRoute.get("/featured",verifyToken,featuredProducts);
 
 
 module.exports=productRoute;
